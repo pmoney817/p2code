a82333bdf1cbcdf75625c4bfcf5a866f0d3b678a
Phatmany Phyathep <phatmany@gmail.com>
2026-04-28 14:21:15 -0500

    Fix footer: use HTML entity for ² instead of tiny BrandName component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -91,7 +91,7 @@ export default function Footer() {
         {/* Bottom bar */}
         <div className="mt-10 pt-6 border-t border-surface-lighter text-center">
           <p className="text-xs text-text-dim">
-            &copy; 2026 <BrandName variant="inline" className="text-xs" />. All rights reserved.
+            &copy; 2026 P&sup2;Code. All rights reserved.
           </p>
         </div>
       </div>
